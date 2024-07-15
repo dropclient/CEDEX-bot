7d07200cdc59b95cbd12a630ac214d0840e231d4
dropclient <offed1337@gmail.com>
2024-07-15 23:33:36 +0300

    Updated for CEDEX bot

--- bot/config/config.py
+++ bot/config/config.py
@@ -8,11 +8,7 @@ class Settings(BaseSettings):
     API_HASH: str
 
     FARM_MINING_ERA: bool = True
-    TAPS: bool = True
-    TAPS_AMOUNT: list[int] = [100, 1000]
     CLAIM_SQUAD_REWARD: bool = True
-    CLAIM_TASKS: bool = True
-    REF_ID: str = ''
 
     USE_PROXY_FROM_FILE: bool = False
 
--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -47,24 +47,12 @@ class Tapper:
                 with_tg = False
                 try:
                     await self.tg_client.connect()
-                    start_command_found = False
-
-                    async for message in self.tg_client.get_chat_history('cexio_tap_bot'):
-                        if (message.text and message.text.startswith('/start')) or (message.caption and message.caption.startswith('/start')):
-                            start_command_found = True
-                            break
-
-                    if not start_command_found:
-                        if settings.REF_ID == '':
-                            await self.tg_client.send_message("cexio_tap_bot", "/start 1717162889191233")
-                        else:
-                            await self.tg_client.send_message("cexio_tap_bot", f"/start {settings.REF_ID}")
                 except (Unauthorized, UserDeactivated, AuthKeyUnregistered):
                     raise InvalidSession(self.session_name)
 
             while True:
                 try:
-                    peer = await self.tg_client.resolve_peer('cexio_tap_bot')
+                    peer = await self.tg_client.resolve_peer('cedex_tap_bot')
                     break
                 except FloodWait as fl:
                     fls = fl.value
@@ -79,9 +67,8 @@ class Tapper:
                 bot=peer,
                 platform='android',
                 from_bot_menu=False,
-                url='https://cexp.cex.io/'
+                url='https://cdxp.cedex.io/'
             ))
-
             auth_url = web_view.url
             tg_web_data = unquote(
                 string=unquote(
@@ -106,7 +93,7 @@ class Tapper:
             shuffle(task_list)
             results = []
             for task_id in task_list:
-                response = await http_client.post(url='https://cexp.cex.io/api/startTask',
+                response = await http_client.post(url='https://cdxp.cedex.io/api/startTask',
                                                   json={'authData': tg_web_data, 'devAuthData': self.user_id,
                                                         'data': {'taskId': task_id}})
                 response_text = await response.text()
@@ -128,7 +115,7 @@ class Tapper:
             shuffle(task_list)
             results = []
             for task_id in task_list:
-                response = await http_client.post(url='https://cexp.cex.io/api/checkTask',
+                response = await http_client.post(url='https://cdxp.cedex.io/api/startTask/api/checkTask',
                                                   json={'authData': tg_web_data, 'devAuthData': self.user_id,
                                                         'data': {'taskId': task_id}})
                 response_text = await response.text()
@@ -150,7 +137,7 @@ class Tapper:
             shuffle(task_list)
             results = []
             for task_id in task_list:
-                response = await http_client.post(url='https://cexp.cex.io/api/claimTask',
+                response = await http_client.post(url='https://cdxp.cedex.io/api/claimTask',
                                                   json={'authData': tg_web_data, 'devAuthData': self.user_id,
                                                         'data': {'taskId': task_id}})
                 response_text = await response.text()
@@ -167,84 +154,9 @@ class Tapper:
             await asyncio.sleep(delay=3)
             return [error] * len(task_list)
 
-    async def getTasks(self, http_client: aiohttp.ClientSession, tg_web_data: str):
-        try:
-            none_tasks = []
-            ready_to_check_tasks = []
-            response = await http_client.post(url='https://cexp.cex.io/api/getUserInfo',
-                                              json={'authData': tg_web_data, 'devAuthData': int(self.user_id),
-                                                    'data': {}, 'platform': 'ios'})
-            response_text = await response.text()
-            response.raise_for_status()
-            data = json.loads(response_text)
-            if data["status"] == "ok":
-                response_text = response_text.replace('\n', '')
-                tasks = json.loads(response_text).get("data", {}).get("tasks", {})
-                for task_id, task_info in tasks.items():
-                    priority = task_info.get("priority", 0)
-                    state = task_info.get("state", "")
-
-                    if 13 <= priority <= 30:
-                        if state == "NONE":
-                            none_tasks.append(task_id)
-                        elif state == "ReadyToCheck":
-                            ready_to_check_tasks.append(task_id)
-                return none_tasks, ready_to_check_tasks
-            else:
-                logger.info(f'{self.session_name} | Непредвиденная ошибка')
-                return [], []
-        except Exception as error:
-            logger.error(f"{self.session_name} | Unknown error when getting tasks, {error}")
-            await asyncio.sleep(delay=3)
-            return [], []
-
-    async def claim_ref(self, http_client: aiohttp.ClientSession, part, tg_web_data: str):
-        try:
-            if part == 1:
-                response = await http_client.post(url='	https://cexp.cex.io/api/getChildren',
-                                              json={'authData': tg_web_data, 'devAuthData': self.user_id,
-                                                    'data': {}})
-                response_text = await response.text()
-                response.raise_for_status()
-                data = json.loads(response_text)
-                return data
-            elif part == 2:
-                response = await http_client.post(url='	https://cexp.cex.io/api/claimFromChildren',
-                                                  json={'authData': tg_web_data, 'devAuthData': self.user_id,
-                                                        'data': {}})
-                response_text = await response.text()
-                response.raise_for_status()
-                data = json.loads(response_text)
-                status = data['status']
-                return data
-        except Exception as error:
-            logger.error(f"{self.session_name} | Unknown error when claiming squad awards, {error}")
-            await asyncio.sleep(delay=3)
-
-            return error
-
-    async def claim_taps(self, http_client: aiohttp.ClientSession, taps, tg_web_data: str) -> bool:
-        try:
-            response = await http_client.post(url='https://cexp.cex.io/api/claimTaps',
-                                              json={'authData': tg_web_data, 'devAuthData': self.user_id,
-                                                    'data': {'taps': taps}})
-            response_text = await response.text()
-            response.raise_for_status()
-            data = json.loads(response_text)
-            if data["status"] == "ok":
-                return True
-            else:
-                logger.info(f'{self.session_name} | Непредвиденная ошибка')
-                return False
-        except Exception as error:
-            logger.error(f"{self.session_name} | Unknown error when claim taps, {error}")
-            await asyncio.sleep(delay=3)
-
-            return False
-
     async def start_farm(self, http_client: aiohttp.ClientSession, tg_web_data: str) -> bool:
         try:
-            response = await http_client.post(url='https://cexp.cex.io/api/startFarm',
+            response = await http_client.post(url='https://cdxp.cedex.io/api/startFarm',
                                               json={'authData': tg_web_data, 'devAuthData': self.user_id,
                                                     'data': {}})
             response_text = await response.text()
@@ -263,7 +175,7 @@ class Tapper:
 
     async def claim_farm(self, http_client: aiohttp.ClientSession, tg_web_data: str):
         try:
-            response = await http_client.post(url='https://cexp.cex.io/api/claimFarm',
+            response = await http_client.post(url='https://cdxp.cedex.io/api/claimFarm',
                                               json={'authData': tg_web_data, 'devAuthData': self.user_id,
                                                     'data': {}})
             response_text = await response.text()
@@ -284,11 +196,12 @@ class Tapper:
 
     async def profile_data(self, http_client: aiohttp.ClientSession, tg_web_data: str):
         try:
-            response = await http_client.post(url='https://cexp.cex.io/api/getUserInfo',
+            response = await http_client.post(url='https://cdxp.cedex.io/api/getUserInfo',
                                               json={'authData': tg_web_data, 'devAuthData': int(self.user_id),
                                                     'data': {}, 'platform': 'ios'})
+
             response_text = await response.text()
-            response.raise_for_status()
+            response.raise_for_status()      
             data = json.loads(response_text)
             if data:
                 return data
@@ -323,46 +236,8 @@ class Tapper:
                     prof_data = await self.profile_data(http_client=http_client, tg_web_data=tg_web_data)
                     if prof_data:
                         pass
-
-                    cooldown = int(prof_data['data']['currentTapWindowFinishAt'])
                     available_taps = int(prof_data['data']['availableTaps'])
                     farm_reward = int(float(prof_data['data']['farmReward']))
-                    data = await self.claim_ref(http_client=http_client, tg_web_data=tg_web_data, part=1)
-                    none_tasks, ready_to_check_tasks = await self.getTasks(http_client=http_client,
-                                                                           tg_web_data=tg_web_data)
-
-                    if settings.CLAIM_TASKS and (none_tasks or ready_to_check_tasks):
-                        none_tasks, ready_to_check_tasks = await self.getTasks(http_client=http_client,
-                                                                               tg_web_data=tg_web_data)
-
-                        statuses = None
-
-                        if none_tasks:
-                            statuses = await self.startTasks(http_client=http_client, task_list=none_tasks,
-                                                         tg_web_data=tg_web_data)
-
-                        if ready_to_check_tasks:
-                            only_check = True
-
-                        if (statuses is not None and all(statuses)) or only_check:
-                            logger.success(f'{self.session_name} | Waiting before claiming, 65 s')
-                            await asyncio.sleep(delay=65)
-                            none_tasks, ready_to_check_tasks = await self.getTasks(http_client=http_client,
-                                                                                   tg_web_data=tg_web_data)
-                            status = await self.checkTasks(http_client=http_client, task_list=ready_to_check_tasks,
-                                                           tg_web_data=tg_web_data)
-                            if all(status):
-                                status = await self.claimTasks(http_client=http_client, task_list=ready_to_check_tasks,
-                                                               tg_web_data=tg_web_data)
-                                if all(status):
-                                    logger.success(f'{self.session_name} | Claimed all tasks')
-
-                    if settings.CLAIM_SQUAD_REWARD and int(float(data['data']['totalRewardsToClaim'])) != 0:
-                        data = await self.claim_ref(http_client=http_client, tg_web_data=tg_web_data, part=2)
-                        if data['status'] == "ok":
-                            logger.success(f'{self.session_name} | Claimed referrals reward, amount: '
-                                        f'{data["data"]["claimedBalance"]}')
-
                     if farm_reward == 0 and settings.FARM_MINING_ERA:
                         status = await self.start_farm(http_client=http_client, tg_web_data=tg_web_data)
                         if status is True:
@@ -376,14 +251,20 @@ class Tapper:
                             current_time = datetime.now()
                             time_conv_rn = current_time.astimezone(timezone.utc)
                             time_conv_rn = time_conv_rn.strftime("%Y-%m-%d %H:%M:%S")
+                            time_conv_test = datetime.strptime(time_conv_rn, "%Y-%m-%d %H:%M:%S")
+
                             farm_start = prof_data['data']['farmStartedAt']
                             convert = datetime.strptime(farm_start, "%Y-%m-%dT%H:%M:%S.%fZ")
                             convert += timedelta(hours=4)
+                            balance = int(float(prof_data['data']['balance']))
+
+                            time_diff = convert - time_conv_test
+                            hours_diff = round(time_diff.total_seconds() / 3600, 1)
                             if time_conv_rn > str(convert):
                                 status = await self.claim_farm(http_client=http_client, tg_web_data=tg_web_data)
                                 if status == "ok":
                                     logger.success(f'{self.session_name} | Claimed mining era, got amount: '
-                                                   f'{farm_reward}')
+                                                   f'{farm_reward} | Balance: {balance}')
                                     status = await self.start_farm(http_client=http_client, tg_web_data=tg_web_data)
                                     if status is True:
                                         prof_data = await self.profile_data(http_client=http_client,
@@ -393,34 +274,17 @@ class Tapper:
                                             f'{self.session_name} | Start mining era, early reward: {farm_reward}')
                                 else:
                                     logger.info(f'{self.session_name} | {status}')
+                            else:                                       
+                                logger.info(f'{self.session_name} | Balance: {balance} | Time until next claim: {hours_diff} hours | Waiting 10 minutes')
+                                await asyncio.sleep(600)  
                         except Exception as e:
                             logger.error(e)
-
-                    if available_taps != 0 and settings.TAPS:
-                        rand_taps = randint(a=settings.TAPS_AMOUNT[0], b=settings.TAPS_AMOUNT[1])
-                        if available_taps < rand_taps:
-                            status = await self.claim_taps(http_client=http_client, taps=available_taps,
-                                                           tg_web_data=tg_web_data)
-                            if status is True:
-                                logger.success(f'{self.session_name} | Claimed taps, count: {available_taps}')
-                                await asyncio.sleep(delay=2)
-                        else:
-                            status = await self.claim_taps(http_client=http_client, taps=rand_taps,
-                                                       tg_web_data=tg_web_data)
-                            if status is True:
-                                logger.success(f'{self.session_name} | Claimed taps, count: {rand_taps}')
-                                await asyncio.sleep(delay=2)
-                    elif cooldown != 0 and available_taps == 0:
-                        logger.info(f'{self.session_name} | No taps, sleeping 1 hour')
-                        await asyncio.sleep(delay=3600)
-
                 except InvalidSession as error:
                     raise error
 
                 except Exception as error:
                     logger.error(f"{self.session_name} | Unknown error: {error}")
                     await asyncio.sleep(delay=3)
-
 
 async def run_tapper(tg_client: Client, proxy: str | None):
     try:
